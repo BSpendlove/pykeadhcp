7af9fa52fba3f9dc7cc44c9417e892c7ae5dafa2
Brandon Spendlove <brandon-spendlove@hotmail.co.uk>
2023-05-29 13:35:22 +0000

    dhcp6 statistic-get test

--- tests/dhcp6/test_kea_dhcp6_statistics.py
+++ tests/dhcp6/test_kea_dhcp6_statistics.py
@@ -4,3 +4,9 @@ from pykeadhcp import Kea
 def test_kea_dhcp6_statistic_get_all(kea_server: Kea):
     response = kea_server.dhcp6.statistic_get_all()
     assert response.result == 0
+
+
+def test_kea_dhcp6_statistic_get(kea_server: Kea):
+    response = kea_server.dhcp6.statistic_get(name="pkt6-received")
+    assert response.result == 0
+    assert response.arguments["pkt6-received"]
