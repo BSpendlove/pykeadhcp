d805a88d3410bf78fad529a90150d8c43f5cae50
Brandon Spendlove <brandon-spendlove@hotmail.co.uk>
2023-05-29 13:20:46 +0000

    dhcp6 subnet6 add tests

---
+++ tests/dhcp6/test_kea_dhcp6_subnet6.py
@@ -0,0 +1,35 @@
+from pykeadhcp import Kea
+from pykeadhcp.models.dhcp6.subnet import Subnet6
+from pykeadhcp.exceptions import KeaSubnetNotFoundException
+import pytest
+
+"""subnet4 process:
+get (non-existent subnet)
+add
+add (again to check duplicate subnets)
+list
+get
+delta-add (partial update code 3 option and valid-lifetime)
+delta-del (remove valid-lifetime)
+update (full update without option 3... should disappear)
+del
+"""
+
+
+def test_kea_dhcp6_subnet6_get_non_existent(kea_server: Kea):
+    with pytest.raises(KeaSubnetNotFoundException):
+        response = kea_server.dhcp6.subnet6_get(subnet_id=40123)
+
+
+def test_kea_dhcp6_subnet6_add(kea_server: Kea):
+    data = Subnet6(id=40123, subnet="2001:db8::/64")
+    subnets = [data]
+    response = kea_server.dhcp6.subnet6_add(subnets=subnets)
+    assert response.result == 0
+
+
+def test_kea_dhcp6_subnet6_add_existing(kea_server: Kea):
+    data = Subnet6(id=40123, subnet="2001:db8::/64")
+    subnets = [data]
+    response = kea_server.dhcp6.subnet6_add(subnets=subnets)
+    assert response.result == 1
