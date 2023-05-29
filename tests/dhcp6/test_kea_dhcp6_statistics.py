37ce0e75fe10f7b56a023765c47020eed574196a
Brandon Spendlove <brandon-spendlove@hotmail.co.uk>
2023-05-29 13:36:00 +0000

    dhcp6 statistic-get bad name test

--- tests/dhcp6/test_kea_dhcp6_statistics.py
+++ tests/dhcp6/test_kea_dhcp6_statistics.py
@@ -10,3 +10,9 @@ def test_kea_dhcp6_statistic_get(kea_server: Kea):
     response = kea_server.dhcp6.statistic_get(name="pkt6-received")
     assert response.result == 0
     assert response.arguments["pkt6-received"]
+
+
+def test_kea_dhcp6_statistic_get_bad_name(kea_server: Kea):
+    response = kea_server.dhcp6.statistic_get(name="bad-name-argument")
+    assert response.result == 0
+    assert response.arguments == {}
