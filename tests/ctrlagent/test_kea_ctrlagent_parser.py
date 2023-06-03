4f4606cd1844252a204318f76454a720b4373a3b
Brandon Spendlove <brandon-spendlove@hotmail.co.uk>
2023-06-03 10:59:38 +0000

    Basic tests implemented for daemon config parsers

---
+++ tests/ctrlagent/test_kea_ctrlagent_parser.py
@@ -0,0 +1,31 @@
+import json
+from pykeadhcp import Kea
+from pykeadhcp.parsers.ctrlagent import CtrlAgentParser
+
+
+def test_kea_ctrlagent_parser_parse_config(kea_server: Kea):
+    cached_config = kea_server.ctrlagent.cached_config
+    parsed = CtrlAgentParser(config=cached_config)
+
+    assert parsed.config.http_host
+    assert parsed.config.http_port
+    assert json.dumps(
+        parsed.config.dict(exclude_none=True, by_alias=True),
+        indent=4,
+        sort_keys=True,
+    )
+
+
+def test_kea_ctrlagent_parser_config_test(kea_server: Kea):
+    cached_config = kea_server.ctrlagent.cached_config
+    parsed = CtrlAgentParser(config=cached_config)
+    config_to_test = {
+        "Control-agent": parsed.config.dict(exclude_none=True, by_alias=True)
+    }
+
+    test_results = kea_server.ctrlagent.config_test(config=config_to_test)
+    assert test_results.result == 0
+
+    cached_config_json = json.dumps(cached_config, indent=4)
+    parsed_config_json = json.dumps(config_to_test, indent=4, sort_keys=True)
+    assert cached_config_json == parsed_config_json
---
+++ tests/dhcp4/test_kea_dhcp4_parser.py
@@ -0,0 +1,31 @@
+import json
+from pykeadhcp import Kea
+from pykeadhcp.parsers.dhcp4 import Dhcp4Parser
+
+
+def test_kea_dhcp4_parser_parse_config(kea_server: Kea):
+    cached_config = kea_server.dhcp4.cached_config
+    parsed = Dhcp4Parser(config=cached_config)
+
+    assert parsed.config.interfaces_config
+    assert parsed.config.control_socket
+    assert json.dumps(
+        parsed.config.dict(exclude_none=True, by_alias=True),
+        indent=4,
+        sort_keys=True,
+    )
+
+
+def test_kea_dhcp4_parser_config_test(kea_server: Kea):
+    cached_config = kea_server.dhcp4.cached_config
+    parsed = Dhcp4Parser(config=cached_config)
+    config_to_test = {"Dhcp4": parsed.config.dict(exclude_none=True, by_alias=True)}
+
+    test_results = kea_server.dhcp4.config_test(config=config_to_test)
+    assert test_results.result == 0
+
+    cached_config_json = json.dumps(cached_config, indent=4)
+    parsed_config_json = json.dumps(config_to_test, indent=4, sort_keys=True)
+
+    print(parsed_config_json)
+    assert cached_config_json == parsed_config_json
