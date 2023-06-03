6c3077b2771f78b904ff41ceece6f601df4679f0
Brandon Spendlove <brandon-spendlove@hotmail.co.uk>
2023-06-03 10:51:42 +0000

    Updated enums for new parsers and fixed variables

--- pykeadhcp/models/enums.py
+++ pykeadhcp/models/enums.py
@@ -30,7 +30,7 @@ class LoggerLevelEnum(str, Enum):
     critical = "CRITICAL"
     error = "ERROR"
     warning = "WARNING"
-    info = "WARNING"
+    info = "INFO"
     debug = "DEBUG"
 
 
@@ -42,3 +42,42 @@ class DHCPSocketTypeEnum(str, Enum):
 class OutboundInterfaceEnum(str, Enum):
     same_as_inbound = "same-as-inbound"
     use_routing = "use-routing"
+
+
+class DatabaseTypeEnum(str, Enum):
+    memfile = "memfile"
+    mysql = "mysql"
+    postgresql = "postgresql"
+
+
+class DatabaseOnFailEnum(str, Enum):
+    stop_retry_exit = "stop-retry-exit"
+    serve_retry_exit = "serve-retry-exit"
+    serve_retry_continue = "serve-retry-continue"
+
+
+class HostReservationIdentifierEnum(str, Enum):
+    duid = "duid"
+    hw_address = "hw-address"
+    circuit_id = "circuit-id"
+    client_id = "client-id"
+    flex_id = "flex-id"
+
+
+class NCRProtocolEnum(str, Enum):
+    udp = "UDP"
+    tcp = "TCP"
+
+
+class NCRFormatEnum(str, Enum):
+    json = "JSON"
+
+
+class ServerIdTypeEnum(str, Enum):
+    llt = "LLT"
+    en = "EN"
+    ll = "LL"
+
+
+class AuthenticationTypeEnum(str, Enum):
+    basic = "basic"
